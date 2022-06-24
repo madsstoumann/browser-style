44ac0e7b04b9993ab9eda2347375ac2721a04e4c
Mads Stoumann <stoumann@gmail.com>
2022-06-24 15:18:49 +0200

    Dark Mode

--- js/form.js
+++ js/form.js
@@ -2,14 +2,13 @@ jsIndeterminate.indeterminate = true;
 
 // const clickOutside = (node) => document.addEventListener('click', (event) => event.target.contains(node));
 
-document.querySelectorAll('select[is="custom"]').forEach(select => select.addEventListener('pointerdown', (event) => {
-	const node = event.target;
-	if (node.tagName === 'OPTION') {
-		// event.target.setAttribute('selected', 'selected')
-		select.value = node.value;
-		select.removeAttribute('size');
-	}
-	else {
-		select.size = 3
-	}
-}))
+// document.querySelectorAll('select[is="custom"]').forEach(select => select.addEventListener('pointerdown', (event) => {
+// 	const node = event.target;
+// 	if (node.tagName === 'OPTION') {
+// 		select.value = node.value;
+// 		select.removeAttribute('size');
+// 	}
+// 	else {
+// 		select.size = 3
+// 	}
+// }))
