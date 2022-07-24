bd7f976df686faecd01f682bc4fe4b3b6f87811a
madsstoumann <stoumann@gmail.com>
2022-07-24 20:01:22 +0300

    Work-in-progress components

--- assets/js/xy.js
+++ assets/js/xy.js
@@ -1,84 +1,66 @@
 /**
  * xy
- * @version 1.0.08
- * @summary 07-11-2022
+ * @version 1.0.10
+ * @summary 07-20-2022
  * @author Mads Stoumann
  * @description XY Controller
 */
-export default function xy(pad, config) {
+export default function xy(elm, config) {
 	const settings = Object.assign({
+		eventScope: elm,
+		gridX: 0,
+		gridY: 0,
 		leave: true,
 		minX: 0,
 		maxX: 100,
 		minY: 0,
 		maxY: 100,
-		eventScope: pad,
 		shift: 10,
-		snapX: 0,
-		snapY: 0,
 		x: 50,
 		y: 50 
 	}, config);
 
-	let active = false, areaX, areaY, maxX, maxY, ratioX, ratioY, xCurrent, yCurrent, x = settings.x, y = settings.y;
+	let active = false, areaX, areaY, maxX, maxY, pointsize, ratioX, ratioY, snapX, snapY, xCurrent, yCurrent, x = settings.x, y = settings.y;
 	const difX = settings.maxX - settings.minX;
 	const difY = settings.maxY - settings.minY;
-	const ro = new ResizeObserver(() => { refresh(); xyset(x, y); checkUpdate()});
-	const point = pad.elements.xyPoint;
-	const pointsize = point.offsetWidth;
+	const ro = new ResizeObserver(() => { refresh(); xyset(x, y); update()});
+	const point = elm.elements.xyPoint;
 
-	pad.addEventListener('keydown', keymove);
-	pad.addEventListener('pointerdown', down);
-	if (settings.leave) pad.addEventListener('pointerleave', () => pad.dispatchEvent(new Event('pointerup')));
-	pad.addEventListener('pointermove', move);
-	pad.addEventListener('pointerup', up);
-	ro.observe(pad);
+	elm.style.setProperty('--xy-grid-x', settings.gridX);
+	elm.style.setProperty('--xy-grid-y', settings.gridY);
+
+	elm.addEventListener('keydown', keymove);
+	elm.addEventListener('pointerdown', down);
+	if (settings.leave) elm.addEventListener('pointerleave', () => elm.dispatchEvent(new Event('pointerup')));
+	elm.addEventListener('pointermove', move);
+	elm.addEventListener('pointerup', up);
+	ro.observe(elm);
 
 	/**
 	 * @function keymove
 	 * @param {Event} e
-	 * @description Handles keyboard-navigation with arrow-keys. `Escape` triggers an “xyup”-event.
+	 * @description Handles keyboard-navigation with arrow-keys.
 	*/
 	function keymove(e) {
-		const valX = e.shiftKey ? settings.shift * ratioX : 1 * ratioX;
-		const valY = e.shiftKey ? settings.shift * ratioY : 1 * ratioY;
-		let x = parseInt(pad.style.getPropertyValue('--tx')||0);
-		let y = parseInt(pad.style.getPropertyValue('--ty')||0);
-		if (e.key !== 'Tab') e.preventDefault();		
+		let valX = e.shiftKey ? e.altKey ? 1 : settings.shift * ratioX : ratioX;
+		const valY = e.shiftKey ? e.altKey ? 1 : settings.shift * ratioY : ratioY;
+		let x = elm.style.getPropertyValue('--tx')-0;
+		let y = elm.style.getPropertyValue('--ty')-0;
+		if (e.key !== 'Tab') e.preventDefault();
+
 		switch(e.key) {
-			case 'ArrowDown': y = y + valY; break;
-			case 'ArrowLeft': x = x - valX; break;
-			case 'ArrowRight': x = x + valX; break;
-			case 'ArrowUp': y = y - valY; break;
+			case 'ArrowDown': if (snapY) { y+= snapY } else { y+= valY }; break;
+			case 'ArrowLeft': if (snapX) { x-= snapX } else { x-= valX }; break;
+			case 'ArrowRight': if (snapX) { x+= snapX } else { x+= valX; }; break;
+			case 'ArrowUp': if (snapY) { y-= snapY } else { y-= valY }; break;
+			case 'End': if (e.ctrlKey) { y = maxY } else { x = maxX }; break;
+			case 'Home': if (e.ctrlKey) { y = 0 } else { x = 0 }; break;
 			case ' ': customEvent('xytoggle'); break;
 			default: break;
 		}
 		xCurrent = x;
 		yCurrent = y;
-		checkUpdate();
-	}
-
-	/**
-	 * @function checkUpdate
-	 * @param {Event} e
-	 * @description Check coordinate boundaries, sets them to max values if they're greater, before calling `update`-method
-	*/
-	function checkUpdate(e) {
-		if (e) {
-			xCurrent = e.offsetX - (pointsize / 2);
-			yCurrent = e.offsetY - (pointsize / 2);
-		}
-
-		/* Snap-to-grid, if enabled */
-		if (settings.snapX) xCurrent = Math.round(xCurrent / settings.snapX) * settings.snapX;
-		if (settings.snapY) yCurrent = Math.round(yCurrent / settings.snapY) * settings.snapY;
-
-		/* Check boundaries: top, right, bottom, left */
-		if (yCurrent < 0) yCurrent = 0;
-		if (xCurrent > maxX) xCurrent = maxX;
-		if (yCurrent > maxY) yCurrent = maxY;
-		if (xCurrent < 0) xCurrent = 0;
-		update(xCurrent, yCurrent);
+		update();
 	}
 
 	/**
@@ -98,18 +80,18 @@ export default function xy(pad, config) {
 	*/
 	function down(e) {
 		active = true;
-		pad.dataset.active = 1;
+		elm.dataset.active = 1;
 		customEvent('xydown');
-		checkUpdate(e);
+		update(e);
 	}
 
 	/**
 	 * @function move
 	 * @param {Event} e
-	 * @description Pointer move, updates current coordinates, calls `checkUpdate`.
+	 * @description Pointer move, updates current coordinates, calls `update`.
 	*/
 	function move(e) {
-		if (active) checkUpdate(e);
+		if (active) update(e);
 	}
 
 	/**
@@ -117,12 +99,15 @@ export default function xy(pad, config) {
 	 * @description Refreshes variables based on element-dimensions, when element is resized.
 	*/
 	function refresh() {
-		areaX = pad.offsetWidth;
-		areaY = pad.offsetHeight;
+		pointsize = point.offsetWidth;
+		areaX = elm.offsetWidth;
+		areaY = elm.offsetHeight;
 		ratioX = ((areaX - pointsize) / difX);
 		ratioY = ((areaY - pointsize) / difY);
 		maxX = areaX - pointsize;
 		maxY = areaY - pointsize;
+		snapX = settings.gridX ? areaX / settings.gridX : 0;
+		snapY = settings.gridY ? areaY / settings.gridY : 0;
 	}
 
 	/**
@@ -131,22 +116,50 @@ export default function xy(pad, config) {
 	*/
 	function up() {
 		active = false;
-		pad.dataset.active = 0;
+		elm.dataset.active = 0;
 		customEvent('xyup');
+	}
+	
+	/**
+	 * @function update
+	 * @param {Event} e
+	 * @description Check coordinate boundaries, sets them to max values if they're greater, before calling `update`-method
+	*/
+	function update(e) {
+		if (e) {
+			xCurrent = e.offsetX - (pointsize / 2);
+			yCurrent = e.offsetY - (pointsize / 2);
+		}
+
+		/* Snap-to-grid, if enabled */
+		if (snapX) xCurrent = Math.round(xCurrent / snapX) * snapX;
+		if (snapY) yCurrent = Math.round(yCurrent / snapY) * snapY;
+
+		/* Check boundaries: top, right, bottom, left */
+		if (yCurrent < 0) yCurrent = 0;
+		if (xCurrent > maxX) xCurrent = maxX;
+		if (yCurrent > maxY) yCurrent = maxY;
+		if (xCurrent < 0) xCurrent = 0;
+
+		updateXY(xCurrent, yCurrent);
 	}
 
 	/**
-	 * @function update
+	 * @function updateXY
 	 * @param {Number} tx
 	 * @param {Number} ty
 	 * @description Updates x and y values from current coordinates. Sets CSS custom properties used for `translate`, dispatches an event with an object.
 	*/
-	function update(tx, ty) {
+	function updateXY(tx, ty) {
 		x = tx * (difX / (areaX - pointsize)) + settings.minX;
 		y = difY - ty * (difY / (areaY - pointsize)) + settings.minY;
-		pad.style.setProperty('--tx', tx);
-		pad.style.setProperty('--ty', ty);
-		customEvent('xymove', { detail: { x, y, tx, ty } });
+		elm.style.setProperty('--tx', tx);
+		elm.style.setProperty('--ty', ty);
+
+		/* if gridX/gridY is used, get current "cell" from tx and ty */
+		const gx = snapX ? (tx / snapX) + 1 : 1;
+		const gy = snapY ? (ty / snapY) + 1 : 1;
+		customEvent('xymove', { detail: { x, y, tx, ty, gx, gy } });
 	}
 
 	/**
