7bc9b2334223a30c2fd5caa1f5bc916af8a5c5c5
Mads Stoumann <stoumann@gmail.com>
2023-09-19 11:36:11 +0200

    scroll updates

--- ui/scroll/uiScroll.js
+++ ui/scroll/uiScroll.js
@@ -3,76 +3,108 @@ export default function uiScroll(scroll, settings = {}) {
   const config = Object.assign({
     scrollActive: '--active',
     scrollAutoPlay: 0,
+    scrollBehavior: 'smooth',
     scrollNav: 'ui-scroll-nav',
     scrollNext: '--icon',
     scrollNextInner: `<ui-icon type="chevron right"></ui-icon>`,
     scrollPrev: '--icon',
     scrollPrevInner: `<ui-icon type="chevron left"></ui-icon>`,
+    scrollResizeThreshold: 10,
   }, settings, scroll.dataset)
+  
   const items = [...scroll.querySelectorAll('& >*')]
   if (!items || !config.scrollNav) return
-  let index = 0, itemsPerPage = 1, pages = 1
-  const [dots, next, prev] = uiScrollNav(scroll, pages, config)
 
-  const resizeObserver = new ResizeObserver(throttle(() => {
-    index = 0;
-    itemsPerPage = Math.floor(scroll.offsetWidth / items[0].offsetWidth);
-    pages = Math.ceil(items.length / itemsPerPage);
-    dots.innerHTML = `<li></li>`.repeat(pages)
-    scrollToPage(0, 'auto')
-  }, 250))
+  const [dots, next, prev] = scrollNav(scroll)
+  const tabs = scroll.parentNode.querySelectorAll('[role=tab]')
+  let index, inlineSize, itemsPerPage, pages
 
-  const scrollToPage = (index, behavior = 'smooth') => {
+  /* Methods */
+  function scrollNav() { 
+    const nav = scroll.nextElementSibling || document.createElement('nav')
+    const dots = nav.querySelector('ol') || document.createElement('ol')
+    const next = nav.querySelector('[data-action=next]') || document.createElement('button')
+    const prev = nav.querySelector('[data-action=prev]') || document.createElement('button')
+  
+    if (!nav.children.length) {
+      next.classList.add(config.scrollNext)
+      next.innerHTML = config.scrollNextInner
+      prev.classList.add(config.scrollPrev)
+      prev.innerHTML = config.scrollPrevInner
+      nav.classList.add(config.scrollNav)
+      if (config.scrollNavModifier) nav.classList.add(config.scrollNavModifier)
+      nav.append(prev, dots, next)
+      scroll.after(nav)
+    }
+    return [dots, next, prev]
+  }
+
+  function scrollToPage(index, behavior = config.scrollBehavior) {
     items[index * itemsPerPage].scrollIntoView({
       behavior, block: 'nearest', inline: 'start'
     })
-    updateUI()
+    updateUI(index)
   }
-  const updateUI = () => {
+
+  function updateData() {
+    index = 0
+    inlineSize = scroll.offsetWidth
+    itemsPerPage = Math.floor(inlineSize / items[0].offsetWidth)
+    pages = Math.ceil(items.length / itemsPerPage)
+    dots.innerHTML = `<li></li>`.repeat(pages)
+  }
+
+  function updateUI(index) {
     prev.disabled = (index === 0)
     next.disabled = (index === pages - 1)
-    Array.from(dots.children).forEach((dot, i) => dot.ariaSelected = i === index)
+    Array.from(dots.children).forEach((dot, current) => dot.ariaSelected = index === current)
     items.forEach((elm, current) => elm.classList.toggle(config.scrollActive, index === current) )
+    tabs.forEach((tab, current) => tab.ariaSelected = index === current)
   }
+
+  /* Event Listeners */
   next.addEventListener('click', () => {
     index++; if (index >= pages) index = 0
     scrollToPage(index)
   })
+
   prev.addEventListener('click', () => {
     index--; if (index < 0) index = pages - 1
     scrollToPage(index)
   })
+
   scroll.addEventListener('scrollend', e => {
     const endOfScroll = scroll.scrollLeft + scroll.offsetWidth >= scroll.scrollWidth
     if (endOfScroll) index = pages - 1
     else index = Math.round(scroll.scrollLeft / scroll.offsetWidth)
-    updateUI()
+    updateUI(index)
   })
 
+  tabs.forEach(tab => {
+    tab.addEventListener('click', event => {
+      event.preventDefault()
+      const index = items.findIndex(panel => panel.id === tab.getAttribute('aria-controls'))
+      if (index > -1) scrollToPage(index)
+    })
+  })
+
+  /* Auto Play */
   if (config.scrollAutoPlay) {
     setInterval(() => {
       index++; if (index >= pages) index = 0
       scrollToPage(index)
     }, parseInt(config.scrollAutoPlay, 10))
   }
+
+  /* Resize Observer / Init */
+  const resizeObserver = new ResizeObserver((entries) => {
+    const entryInlineSize = Math.floor(entries[0].contentBoxSize[0].inlineSize)
+    if (Math.abs(entryInlineSize - inlineSize) > parseInt(config.scrollResizeThreshold, 10)) {
+      updateData()
+      scrollToPage(0)
+    }
+  })
   resizeObserver.observe(scroll)
-}
-
-function uiScrollNav(node, items, config = {}) { 
-  const nav = node.nextElementSibling || document.createElement('nav')
-  const dots = nav.querySelector('ol') || document.createElement('ol')
-  const next = nav.querySelector('[data-action=next]') || document.createElement('button')
-  const prev = nav.querySelector('[data-action=prev]') || document.createElement('button')
-
-  if (!nav.children.length) {
-    next.classList.add(config.scrollNext)
-    next.innerHTML = config.scrollNextInner
-    prev.classList.add(config.scrollPrev)
-    prev.innerHTML = config.scrollPrevInner
-    nav.classList.add(config.scrollNav)
-    if (config.scrollNavModifier) nav.classList.add(config.scrollNavModifier)
-    nav.append(prev, dots, next)
-    node.after(nav)
-  }
-  return [dots, next, prev]
+  updateData() 
+  scrollToPage(0)
 }
