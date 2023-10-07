8e9f22f10795db335a4d4012f900be00e5a6203f
Mads Stoumann <stoumann@gmail.com>
2023-10-07 11:55:27 +0200

    AutoSuggest

--- assets/js/datasetWithTypes.js
+++ assets/js/datasetWithTypes.js
@@ -2,7 +2,12 @@ export default function datasetWithTypes(dataset) {
 	if (dataset) {
 		const obj = {}
 		Object.keys(dataset).forEach(key => {
-			obj[key] = JSON.parse(dataset[key])
+			try {
+				obj[key] = JSON.parse(dataset[key])
+			}
+			catch (e) {
+				obj[key] = dataset[key]
+			}
 		})
 		return obj;
 	}
--- ui/autosuggest/uiAutoSuggest.js
+++ ui/autosuggest/uiAutoSuggest.js
@@ -9,32 +9,32 @@ export default function uiAutoSuggest(input, args) {
 	const settings = Object.assign({
 		api: '',
 		apiCache: false,
-		apiKey: '',
+		arrayKey: '',
+		callback: '',
 		invalid: 'Not a valid selection',
-		limit: false
-	}, datasetWithTypes(args));
+		limit: false,
+		objectKey: ''
+	}, (typeof args === 'object') ? args : datasetWithTypes(input.dataset || {}));
 
 	let data = [];
-
-	/* Only allow input.value to match an item from the list */
+	/* If `limit === true`: input.value MUST match an item from the list */
 	const limit = () => {
 		const option = selected();
 		input.setCustomValidity(option ? '' : settings.invalid);
 		if (!input.checkValidity()) {
 			input.reportValidity();
-			console.log('invalid');
 		}
 		else {
-			console.log('valid');
+			input.setCustomValidity('')
 		}
 	}
 
-	/* event “oninput” */
+	/* `onentry` is triggered via debounce on event `oninput` */
 	const onentry = async function(event) {
 		const value = input.value.length >= input.minLength && input.value.toLowerCase();
 		if (!value) return;
 
-		/* “onselect” for datalist, work for both clicks and “Enter”: */
+		/* `onselect` for datalist, work for both clicks and “Enter”: */
 		if (event.inputType == "insertReplacementText" || event.inputType == null) {
 			const option = selected();
 			if (option) {
@@ -47,14 +47,18 @@ export default function uiAutoSuggest(input, args) {
 		/* If data-array is empty, or data should be cached (api will only get contacted once) */
 		if (!data.length || settings.apiCache === false) {
 			data = await (await fetch(settings.api + encodeURIComponent(value))).json();
-			list.innerHTML = data.map(obj => `<option value="${obj[settings.apiKey]}" data-obj='${obj ? JSON.stringify(obj):''}'>`).join('')
+			if (settings.arrayKey) data = data[settings.arrayKey];
+			if (settings.callback && typeof settings.callback === 'function') {
+				return settings.callback(list, data);
+			}
+			list.innerHTML = data.map(obj => `<option value="${obj[settings.objectKey]}" data-obj='${obj ? JSON.stringify(obj):''}'>`).join('')
 		}
 	}
 
-	/* empty data-array, set list to empty `<option>` */
+	/* Empty data-array, set list to empty `<option>` */
 	const reset = () => { data = []; list.innerHTML = `<option value="">`;  input.setCustomValidity('') }
 
-	/* returns option from list, that matches current input.value */
+	/* Returns option from list, that matches current input.value */
 	const selected = () => {
 		const option = [...list.options].filter(entry => entry.value === input.value);
 		return option.length === 1 ? option[0] : 0;
