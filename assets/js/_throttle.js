668a1c70ccea1a8fa1dff2552933f9681d172423
Mads Stoumann <stoumann@gmail.com>
2023-10-15 15:54:43 +0200

    Updated to core

--- assets/js/_throttle.js
+++
@@ -1,7 +0,0 @@
-export default function throttle(f, delay) {
-	let timer = 0;
-	return function(...args) {
-    clearTimeout(timer);
-    timer = setTimeout(() => f.apply(this, args), delay);
-	}
-}
