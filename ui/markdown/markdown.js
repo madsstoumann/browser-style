7d24014bc0b1fe741cf9467ea1764c53fec9f71d
Mads Stoumann <stoumann@gmail.com>
2023-11-10 12:08:01 +0100

    Markdown updates

--- ui/markdown/markdown.js
+++ ui/markdown/markdown.js
@@ -3,7 +3,8 @@ export function htmlToMarkdown(html) {
 	const doc = parser.parseFromString(html, 'text/html');
 	Object.keys(markdown).forEach(tag => {
 		[...doc.body.getElementsByTagName(tag)].forEach(node => {
-			node.innerHTML = markdown[tag](node)
+			try { node.innerHTML = markdown[tag](node) } 
+			catch (err) { console.log(err) }
 		})
 	})
 	return doc.body.textContent.trim()
@@ -11,14 +12,15 @@ export function htmlToMarkdown(html) {
 
 export function markdownToHtml(str) {
 	Object.keys(html).forEach(tag => {
-		str = str.replaceAll(html[tag].re, html[tag].fn || ((_match, text) => text ? `<${tag}>${text}</${tag}>`:''))
+		try {
+			str = str.replaceAll(html[tag].re, html[tag].fn || ((_match, text) => text ? `<${tag}>${text}</${tag}>`:'')) } 
+		catch (err) { console.log(err) }
 	})
 	return str
 }
 
-const H = (prefix, node, suffix) => `${prefix}${node.innerHTML}${suffix === 0 ? '' : suffix || prefix }`;
-
 /* HTML to Markdown */
+const H = (prefix, node, suffix) => `${prefix}${node.innerHTML}${suffix === 0 ? '' : suffix || prefix }`
 const markdown = {
 	a: n => `[${n.innerHTML}](${n.href})`,
 	b: n => H('**', n),
@@ -26,6 +28,7 @@ const markdown = {
 	br: () => `  \r\n`,
 	code: n => H('`', n),
 	del: n => H('---', n),
+	em: n => H('*', n),
 	h1: n => H('# ', n, 0),
 	h2: n => H('## ', n, 0),
 	h3: n => H('### ', n, 0),
@@ -40,6 +43,8 @@ const markdown = {
 	p: n => H('\r\n\r\n', n),
 	pre: n => H('\r\n```\r\n', n),
 	s: n => H('~~', n),
+	strong: n => H('**', n),
+	style: n => ``, // ignore
 	sub: n => H('--', n),
 	sup: n => H('^^', n),
 	table: n => `${[...n.querySelectorAll('tr')].map((row, index) => {
@@ -52,6 +57,7 @@ const markdown = {
 }
 
 /* Markdown to HTML */
+const T = s => s.replace(/</g, '&lt;').replace(/>/g, '&gt;')
 const html = {
 	img: { re: /!\[(.*)\]\((.*)\)/gi, fn: (_match, alt, src) => `\r\n<img src="${src}" alt="${alt}">\r\n` },
 	a: { re: /\[(.*)\]\((.*)\)/gi, fn: (_match, title, href) => `<a href="${href}">${title}</a>` },
@@ -62,9 +68,9 @@ const html = {
 	br: { re: /(  \n)/gi, fn: () => `<br>` },
 	pre: {
 		re: /\n((```|~~~).*\n?([^]*?)\n?\2|((    .*?\n)+))/g,
-		fn: (_match, _g0, _g1, text) => `<pre>${text.replace(/\</g, '&lt;').replace(/>/g, '&gt;')}</pre>\r\n`
+		fn: (_match, _g0, _g1, text) => `<pre>${T(text)}</pre>\r\n`
 	},
-	code: { re: /`(.*?)`/gi },
+	code: { re: /`(.*?)`/g, fn: (_match, text) => `<code>${T(text)}</code>` },
 	h: {
 		re: /(^#{1,6}) (.*)\n/gm,
 		fn: (_match, tag, text) => `<h${tag.length}>${text}</h${tag.length}>\r\n`
@@ -87,10 +93,14 @@ const html = {
 	u: { re: /__(.*)__/gi },
 	em: { re: /_(.*?)_/gi },
 	list: {
-		re: /\n( *)(?:[*\-+]|((\d+)|([a-z])|[A-Z])[.)]) +([^]*?)(?=(\n|$){2})/g,
-		fn: (_match, _, isOL) => isOL ? `\r\n<ol>${_match}\r\n</ol>\r\n` : `\r\n<ul>${_match}\r\n</ul>\r\n`
+		re: /^[0-9-+*]+[ .][\s\S]*?\n{2}/gm, fn: (list) => {
+			const tag = parseInt(list.charAt(0)) > 0 ? 'ol' : 'ul'
+			return `<${tag}>${
+				list.replace(/^[\t0-9-+*]+[ .](.*)\n/gm, (_match, text) => {
+					return `<li>${text}</li>`
+				})
+			}</${tag}>`
+		}
 	},
-	ol: { re: /(^\d+)\.\s(.*)/gm, fn: (_match, index, text) => `<li value="${index}">${text}</li>` },
-	ul: { re: /\-\s(.*)/gi, fn: (_match, text) => `<li>${text}</li>` },
 	p: { re: /\n\n(.*?)\n\n/gi },
 }
--- ui/markdown/tmp.js
+++
@@ -1,2 +0,0 @@
-blockquote: (node) => `\r\n> ${node.innerHTML.replace(/\r|\n/g, '\r\n> ')}\r\n`,
-code: (node) => `\`${node.innerHTML.replace(/\r|\n/g, '').replace(/^\s*(.*)\s*$/, '$1')}\``,
