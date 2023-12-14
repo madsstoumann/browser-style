6fcad6a1fac3548531417650be2d0156b23338ca
Mads Stoumann <stoumann@gmail.com>
2023-12-14 08:57:31 +0000

    RTL fixes

--- RTL-fixes.md
+++
@@ -1,6 +0,0 @@
-RTL
-combobox > input type search (icon + reset)
-anchor
-range-arc
-range-gauge
-tooltip
--- ui/anchor/uiAnchor.js
+++ ui/anchor/uiAnchor.js
@@ -3,23 +3,29 @@ export default function uiAnchor() {
 		document.querySelectorAll('[anchor]').forEach(popover => {
 			const anchor = document.getElementById(popover.getAttribute('anchor'));
 			if (anchor) {
+				/* Initial positioning */
+				setAnchorRect(popover, anchor)
+				setPopoverRect(popover)
+				/* Event listeners */
 				popover.addEventListener("beforetoggle", (event) => {
-					if (event.newState === 'open') {
-						const anchorRect = anchor.getBoundingClientRect();
-						popover.style.setProperty('--anchor-top', anchorRect.top + 'px');
-						popover.style.setProperty('--anchor-left', anchorRect.left + 'px');
-						popover.style.setProperty('--anchor-height', anchorRect.height + 'px');
-						popover.style.setProperty('--anchor-width', anchorRect.width + 'px');
-					}
+					if (event.newState === 'open') setRect(popover, anchor)
 				})
 				popover.addEventListener("toggle", (event) => {
-					if (event.newState === 'open') {
-						const popoverRect = popover.getBoundingClientRect();
-						popover.style.setProperty('--popover-height', popoverRect.height + 'px');
-						popover.style.setProperty('--popover-width', popoverRect.width + 'px');
-					}
+					if (event.newState === 'open') setPopoverRect(popover)
 				})
 			}
 		})
 	}
+}
+function setAnchorRect(popover, anchor) {
+	const anchorRect = anchor.getBoundingClientRect();
+	popover.style.setProperty('--anchor-top', anchorRect.top + 'px');
+	popover.style.setProperty('--anchor-left', anchorRect.left + 'px');
+	popover.style.setProperty('--anchor-height', anchorRect.height + 'px');
+	popover.style.setProperty('--anchor-width', anchorRect.width + 'px');
+}
+function setPopoverRect(popover) {
+	const popoverRect = popover.getBoundingClientRect();
+	popover.style.setProperty('--popover-height', popoverRect.height + 'px');
+	popover.style.setProperty('--popover-width', popoverRect.width + 'px');
 }
