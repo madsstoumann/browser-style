aef72e97766fe0dd2ae7e1a75846c720d3115d0a
Mads Stoumann <stoumann@gmail.com>
2023-12-15 09:04:31 +0000

    RTL fixes

--- ui/word-replace/uiWordReplace.js
+++ ui/word-replace/uiWordReplace.js
@@ -1,36 +1,10 @@
 export default function uiWordReplace(node) {
-	const ul = node.querySelector("ul");
+	const ul = node.querySelector(".ui-word-replace ul");
 	if (!ul) return;
 	const length = ul.children.length;
 	node.style.setProperty("--_length", length);
 	
-	if (node.classList.contains('--spin')) {
-		[...ul.children].forEach((li, index) => {
-			li.style.setProperty("--_index", index);
-			// const keyframes = [
-			// 	{
-			// 		offset: (1/(100/length)),
-			// 		opacity: 1,
-			// 		translate: 0,
-			// 	},
-			// 	{
-			// 		offset: (1/(100/length/2)),
-			// 		opacity: 0,
-			// 		translate: "0 -1lh",
-			// 	},
-			// 	{
-			// 		offset: 1,
-			// 		opacity: 0
-			// 	}
-			// ]
-			
-			// li.animate(keyframes,
-			// 	{
-			// 		duration: 1000 * length,
-			// 		easing: "cubic-bezier(0.075, 0.82, 0.165, 1)",
-			// 		iterations: Infinity,
-			// 	}
-			// )
-		})
-	}
+	[...ul.children].forEach((li, index) => {
+		li.style.setProperty("--_index", index);
+	})
 }
