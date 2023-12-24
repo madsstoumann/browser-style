4fcefa1d5a842ec8d0bca0dd1a6e2288fc1a681f
Mads Stoumann <stoumann@gmail.com>
2023-12-24 11:08:24 +0100

    Reaction

--- ui/reaction/uiReaction.js
+++ ui/reaction/uiReaction.js
@@ -2,7 +2,7 @@ export default function uiReaction(node) {
   const button = node.querySelector('button')
   node.addEventListener('click', e => {
     if (e.target.value) {
-      button.textContent = e.target.value
+      button.firstElementChild.innerHTML = `<text x="0" y="21">${e.target.value}</text>`
     }
   })
 }
