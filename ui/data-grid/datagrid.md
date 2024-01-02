47074a1dae1ffb000b4d9d0f7dbad8d398e30c0e
Mads Stoumann <stoumann@gmail.com>
2024-01-02 16:25:56 +0100

    Updates to data-grid

---
+++ ui/data-grid/datagrid.md
@@ -0,0 +1,20 @@
+## Keyboard Navigation
+
+- `Right Arrow`: Moves focus one cell to the right. If focus is on the right-most cell in the row, focus does not move.
+- `Left Arrow`: Moves focus one cell to the left. If focus is on the left-most cell in the row, focus does not move.
+- `Down Arrow`: Moves focus one cell down. If focus is on the bottom cell in the column, focus does not move.
+- `Up Arrow`: Moves focus one cell up. If focus is on the top cell in the column, focus does not move.
+- `Page Down`: Moves focus down an author-determined number of rows, typically scrolling so the bottom row in the currently visible set of rows becomes one of the first visible rows. If focus is in the last row of the grid, focus does not move.
+- `Page Up`: Moves focus up an author-determined number of rows, typically scrolling so the top row in the currently visible set of rows becomes one of the last visible rows. If focus is in the first row of the grid, focus does not move.
+- `Home`: Moves focus to the first cell in the row that contains focus.
+- `End`: Moves focus to the last cell in the row that contains focus.
+- `Command/Control + Home`: Moves focus to the first cell in the first row.
+- `CommandControl + End`: Moves focus to the last cell in the last row.
+- `Shift + Home`: Moves focus to the first row in the column that contains focus
+- `Shift + End`: Moves focus to the last row in the column that contains focus
+
+
+- `Space`: If selected cell is a sortable header, sorts current column.
+- `Shift + Space`: Selects current row
+- `Command/Control + a`: Selects all visible cells
+- `Command/Control + Shift + i`: Inverts selection
--- ui/data-grid/uiDataGrid.js
+++ ui/data-grid/uiDataGrid.js
@@ -3,15 +3,15 @@ export default class uiDataGrid extends HTMLElement {
 	constructor() {
 		super()
 
+		this.labels = {
+			selectRow: 'Select row',
+			toggleRows: 'Toggle all rows',
+		}
+
 		this.options = {
-			// density: this.getAttribute('density') || 'normal',
 			editable: this.hasAttribute('editable') || false,
 			locale: this.getAttribute('locale') || 'en-US',
-			navigable: this.hasAttribute('navigable') || false,
-			// resizable: this.hasAttribute('resizable') || false,
-			/*searchfilter: this.getAttribute('searchfilter') || 'includes',*/ /* includes | startsWith | endsWith */
 			selectable: this.hasAttribute('selectable') || false,
-			// sortable: this.hasAttribute('sortable') || false,
 			src: this.getAttribute('src') || '',
 		}
 
@@ -31,16 +31,6 @@ export default class uiDataGrid extends HTMLElement {
 	}
 
 	async connectedCallback() {
-		// this.elements = {
-		
-		// 	fieldset: this.querySelector('fieldset'),
-		// 	table: this.querySelector('table'),
-		//  tbody: this.querySelector('tbody'),
-		//	thead: this.querySelector('thead'),
-		// }
-
-
-		this.fieldset = this.querySelector('fieldset')
 		this.table = this.querySelector('table')
 		
 		if (!this.table) {
@@ -64,40 +54,64 @@ export default class uiDataGrid extends HTMLElement {
 
 		// console.log(this.state)
 
-		if (this.options.navigable) {
-			// console.log('add tabindex')
-		}
 
-		if (this.fieldset) {
 
-			this.fieldset.addEventListener('change', event => {
-				const { target } = event;
-				const inputs = Array.from(this.fieldset.querySelectorAll('input:not([data-toggle])'));
-				const toggle = this.fieldset.querySelector('[data-toggle]');
+		// if (this.fieldset) {
 
-				if (target === toggle) {
-					inputs.forEach(input => input.checked = toggle.checked);
-				} else {
-					const allChecked = inputs.every(input => input.checked);
-					toggle.checked = allChecked;
-					toggle.indeterminate = !allChecked && inputs.some(input => input.checked);
-				}
-			})
+		// 	this.fieldset.addEventListener('change', event => {
+		// 		const { target } = event;
+		// 		const inputs = Array.from(this.fieldset.querySelectorAll('input:not([data-toggle])'));
+		// 		const toggle = this.fieldset.querySelector('[data-toggle]');
 
-		}
+		// 		if (target === toggle) {
+		// 			inputs.forEach(input => input.checked = toggle.checked);
+		// 		} else {
+		// 			const allChecked = inputs.every(input => input.checked);
+		// 			toggle.checked = allChecked;
+		// 			toggle.indeterminate = !allChecked && inputs.some(input => input.checked);
+		// 		}
+		// 	})
+		// }
 
-		/* NAVIGATION HANDLER */
-		this.table.addEventListener('click', event => {
-			const { target } = event;
-			if (!target.nodeName === 'TD') return
-			this.state.cellIndex = target.cellIndex
-			this.state.rowIndex = target.parentNode.rowIndex -1;
+		/* Events */
+		this.table.addEventListener('click', (event) => {
+			const node = event.target
+			if (['TD', 'TH'].includes(node.nodeName)) {
+				this.state.cellIndex = node.cellIndex
+				this.state.rowIndex = node.parentNode.rowIndex
+				if (this.state.rowIndex === 0) this.setAttribute('sortindex', this.state.cellIndex);
+				this.setActive()
+			}
 		})
 
-		this.table.addEventListener('keydown', event => {
+		this.table.addEventListener('keydown', (event) => {
+			const node = event.target
 			switch(event.key) {
+				case ' ': /* Space */
+					if (node.nodeName === 'TH') {
+						event.preventDefault();
+						const index = node.cellIndex
+						if (index !== undefined) this.setAttribute('sortindex', this.options.selectable ? index - 1 : index);
+					}
+					if (node.nodeName === 'TD') {
+						if (event.shiftKey) {
+							node.parentNode.toggleAttribute('aria-selected')
+						}
+					}
+					break;
+				case 'a':
+					if (event.ctrlKey || event.metaKey) {
+						event.preventDefault();
+						Array.from(this.table.tBodies[0].rows).forEach(row => row.setAttribute('aria-selected', 'true'))
+					}
+					break;
+				case 'i':
+					if ((event.ctrlKey || event.metaKey) && event.shiftKey) {
+						event.preventDefault();
+						Array.from(this.table.tBodies[0].rows).forEach(row => row.toggleAttribute('aria-selected'))
+					}
 				case 'ArrowDown':
-					this.state.rowIndex = Math.min(this.state.rowIndex + 1, this.state.itemsPerPage - 1)
+					this.state.rowIndex = Math.min(this.state.rowIndex + 1, this.state.itemsPerPage)
 					break;
 				case 'ArrowUp':
 					this.state.rowIndex = Math.max(this.state.rowIndex - 1, 0)
@@ -109,12 +123,12 @@ export default class uiDataGrid extends HTMLElement {
 					this.state.cellIndex = Math.max(this.state.cellIndex - 1, 0)
 					break;
 				case 'End':
-					this.state.cellIndex = this.state.cols - 1
-					if (event.ctrlKey) this.state.rowIndex = this.state.itemsPerPage - 1
+					if (!event.shiftKey) this.state.cellIndex = this.state.cols - 1
+					if ((event.ctrlKey || event.metaKey) || event.shiftKey) this.state.rowIndex = this.state.itemsPerPage
 					break;
 				case 'Home':
-					this.state.cellIndex = 0
-					if (event.ctrlKey) this.state.rowIndex = 0
+					if (!event.shiftKey) this.state.cellIndex = 0
+					if ((event.ctrlKey || event.metaKey) || event.shiftKey) this.state.rowIndex = 0
 					break;
 				case 'PageDown':
 					this.nextPage()
@@ -122,8 +136,8 @@ export default class uiDataGrid extends HTMLElement {
 				case 'PageUp':
 					this.prevPage()
 					break;
-			}
-			this.active()
+				}
+			this.setActive()
 		})
 	}
 
@@ -131,6 +145,7 @@ export default class uiDataGrid extends HTMLElement {
 		if (name === 'itemsperpage') {
 			this.setAttribute('page', 0)
 			this.state.itemsPerPage = parseInt(newValue, 10)
+			if (this.state.itemsPerPage === -1) this.state.itemsPerPage = this.state.rows
 			this.state.pages = this.pages(this.state.rows, this.state.itemsPerPage)
 			this.renderTBody()
 		}
@@ -154,18 +169,22 @@ export default class uiDataGrid extends HTMLElement {
 		}
 		if (name === 'sortorder') {
 			this.state.sortOrder = parseInt(newValue, 10)
-			this.renderTBody()
 		}
   }
 
 	/* Methods */
-	active = () => this.table.tBodies[0].rows[this.state.rowIndex].cells[this.state.cellIndex].focus()
-	camelCase = str => str.split(' ').map((e,i) => i ? e.charAt(0).toUpperCase() + e.slice(1).toLowerCase() : e.toLowerCase()).join('')
+	// camelCase = str => str.split(' ').map((e,i) => i ? e.charAt(0).toUpperCase() + e.slice(1).toLowerCase() : e.toLowerCase()).join('')
 	colGroup = () => this.colgroup.innerHTML = `<col>`.repeat(this.state.cols)
 	nextPage = () => this.setAttribute('page', Math.min(this.state.page + 1, this.state.pages))
 	pages = (items, itemsPerPage) => Math.ceil(items / itemsPerPage)
 	prevPage = () => this.setAttribute('page', Math.max(this.state.page - 1, 0))
 	paginate = data => data.slice(this.state.page * this.state.itemsPerPage, this.state.page * this.state.itemsPerPage + this.state.itemsPerPage)
+	setActive = () => {
+		if (this.active) this.active.setAttribute('tabindex', '-1')
+		this.active = this.table.rows[this.state.rowIndex].cells[this.state.cellIndex]
+		this.active.setAttribute('tabindex', '0')
+		this.active.focus()
+	}
 
 	async fetchData(url) {
 		const data = await (await fetch(url)).json();
@@ -186,53 +205,57 @@ export default class uiDataGrid extends HTMLElement {
 	}
 
 	renderTBody() {
-		if (!this.state.tbody.length) return
+		if (!this.state.tbody.length) return;
 
-		const method = this.getAttribute('searchfilter') || 'includes'
-		const searchterm = this.getAttribute('searchterm')?.toLowerCase()
-		const data = searchterm ? this.state.tbody.filter(row => row.some(cell => typeof cell === 'string' && cell.toLowerCase()[method](searchterm))) : this.state.tbody
+		const method = this.getAttribute('searchfilter') || 'includes';
+		const searchterm = this.getAttribute('searchterm')?.toLowerCase();
+		const data = searchterm
+			? this.state.tbody.filter((row) =>
+					row.some(
+						(cell) =>
+							typeof cell === 'string' && cell.toLowerCase()[method](searchterm),
+					),
+				)
+			: this.state.tbody;
 		const regex = searchterm ? new RegExp(searchterm, 'gi') : null;
-
+	
 		/* Optional: Sort Data */
 		if (this.state.sortIndex > -1) {
-			data.sort((a, b) => typeof a[this.state.sortIndex] === 'string' && a[this.state.sortIndex].localeCompare(b[this.state.sortIndex], 
-				this.options.locale, { sensitivity: 'variant' }
-			))
-			if (this.state.sortOrder === 1) data.reverse()
+			data.sort(
+				(a, b) =>
+					typeof a[this.state.sortIndex] === 'string' &&
+					a[this.state.sortIndex].localeCompare(
+						b[this.state.sortIndex],
+						this.options.locale,
+						{ sensitivity: 'variant' },
+					),
+			);
+			if (this.state.sortOrder === 1) data.reverse();
 		}
-
-		this.table.tBodies[0].innerHTML = this.paginate(data).map(row => {
-			const mapped = row.map((cell, index) => {
-				const td = regex ? cell.replaceAll(regex, match => `<mark>${match}</mark>`) : cell;
-				const TD = `<td${this.options.navigable ? ` tabindex="-1"`: ''}>${td}</td>`
-				const content = (this.options.selectable && index === 0) ? `<td><label><input type="checkbox"></label></td>${TD}` : TD;
-				return content;
-			}).join('')
-			return `<tr>${mapped}</tr>`
-		}).join('')
-		console.log(this.state)
+	
+		this.table.tBodies[0].innerHTML = this.paginate(data)
+			.map((row) => {
+				const mapped = row
+					.map(cell => {
+						return`<td tabindex="-1">${
+							regex
+								? cell.replaceAll(regex, (match) => `<mark>${match}</mark>`)
+								: cell
+						}</td>`;
+					})
+					.join('');
+				return `<tr>${mapped}</tr>`;
+			})
+			.join('');
+		// console.log(this.state);
 	}
 
 	renderTHead() {
-		this.table.tHead.innerHTML = `<tr>${this.state.thead.map((cell, index) => {
-			const th = cell.label || cell
-			const content = (this.options.selectable && index === 0) ? `<th><label><input type="checkbox" data-toggle></label></th><th tabindex="0">${th}</th>` : `<th tabindex="0">${th}</th>`;
-			return content
-		}).join('')}</tr>`;
-
-		[...this.table.tHead.rows].forEach(row => [...row.cells].forEach((cell, index) => {
-			// cell.setAttribute('aria-sort', 'none');
-			// cell.tabIndex = 0;
-			cell.addEventListener('click', event => {
-				this.setAttribute('sortindex', event.target.cellIndex)
-			});
-			cell.addEventListener('keydown', event => {
-				if (event.key === ' ') {
-					event.preventDefault();
-					this.setAttribute('sortindex', event.target.cellIndex)
-				}
+		this.table.tHead.innerHTML = `<tr>${this.state.thead
+			.map((cell, index) => {
+				return `<th tabindex="${index === 0 ? 0 : -1}"><span>${cell.label || cell}</span></th>`
 			})
-		}));
+			.join('')}</tr>`;
 	}
 
 	arrayFromTable(table) {
