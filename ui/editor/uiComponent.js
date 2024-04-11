e6128873ca01498aa3409a80ddc7a044ce9618ba
Mads Stoumann <stoumann@gmail.com>
2024-04-11 09:09:50 +0200

    removed editor

--- ui/editor/uiComponent.js
+++
@@ -1,6 +0,0 @@
-class uiComponent extends HTMLElement { 
-	connectedCallback() {
-		document.body.dispatchEvent(new CustomEvent('uiComponentConnected', {detail: {component: this}}));
-	}
-}
-customElements.define('ui-component', uiComponent);
