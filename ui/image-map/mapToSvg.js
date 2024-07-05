9b65cd17d046d9f4fed303e49cd536126e5271c8
Mads Stoumann <stoumann@gmail.com>
2024-07-05 12:06:05 +0200

    image-map

---
+++ ui/image-map/mapToSvg.js
@@ -0,0 +1,49 @@
+export default function mapToSvg(node) {
+	const map = node.querySelector('map');
+	const img = document.querySelector(`[usemap="#${map.name}"]`);
+	if (!img) return;
+	img.removeAttribute('usemap');
+
+	const areas = [...map.children].map(area => {
+		const id = area.getAttribute('href').replace('#', '');
+
+		const elm = document.getElementById(id);
+		if (elm) {
+			elm.setAttribute('popover', '');
+		}
+
+		switch (area.shape) {
+			case 'circle': {
+				const [cx, cy, r] = area.coords.split(',');
+				return `<circle cx="${cx}" cy="${cy}" r="${r}" popovertarget="${id}" />`;
+			}
+			case 'poly': {
+				const points = area.coords.split(',');
+				const pointsStr = points.reduce((acc, cur, i) => {
+					if (i % 2 === 0) {
+						return `${acc} ${cur},`;
+					} else {
+						return `${acc}${cur} `;
+					}
+				}, '').trim();
+				return `<polygon points="${pointsStr}" popovertarget="${id}" />`;
+			}
+			case 'rect': {
+				const [x, y, width, height] = area.coords.split(',');
+				return `<rect x="${x}" y="${y}" width="${width}" height="${height}" popovertarget="${id}" />`;
+			}
+		}
+	}).join('');
+
+	map.outerHTML = `<svg viewBox="0 0 ${img.getAttribute('width') || img.naturalWidth} ${img.getAttribute('height') || img.naturalHeight}">${areas}</svg>`;
+
+	const popovers = node.querySelectorAll('[popovertarget]');
+	popovers.forEach(popover => {
+		popover.addEventListener('click', () => {
+			const target = document.getElementById(popover.getAttribute('popovertarget'));
+			if (target) {
+				target.showPopover();
+			}
+		});
+	});
+}
