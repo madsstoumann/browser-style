c0a95e7e20b376bcece3060161442573b14589b3
Mads Stoumann <stoumann@gmail.com>
2024-08-06 10:09:07 +0200

    Nataraja

--- art/nataraja/index.js
+++ art/nataraja/index.js
@@ -5,17 +5,14 @@ import random from '/assets/js/random.js';
 import GuiControl from '/ui/gui-control/index.js';
 
 const GUI = document.querySelector('gui-control');
-const storageKey = 'mesh';
+const storageKey = 'natraja';
 const svg = document.getElementById('svg');
 
 GUI.addRange('Columns', 20, '', { min: 1, max: 50, value: 10, name: 'columns' });
 GUI.addRange('Rows', 20, '', { min: 1, max: 50, value: 10, name: 'rows' });
-GUI.addRange('Skew', -40, '', { min: -65, max: 65, name: 'skew' });
-
+GUI.addRange('Skew', -35, '', { min: -45, max: 45, name: 'skew' });
 GUI.addColor('Start Hue', '#ff0000', '', { name: 'starthue' });
 GUI.addColor('End Hue', '#ff0080', '', { name: 'endhue' });
-
-
 commonConfig(GUI);
 GUI.addEventListener('gui-input', (event) => handleGuiEvent(event, svg, GUI, storageKey, nataraja));
 init(GUI, storageKey, []);
@@ -32,25 +29,21 @@ function nataraja(svg, controls) {
 
 	const colWidth = width / columns;
 	const rowHeight = height / rows;
+	const totalRows = rows * 2;
 
 	svg.innerHTML = [...new Array(columns)].map((_col, colIndex) => {
-		const sign = skew > 0 ? -1 : 1;
-		const translation = Math.abs(colIndex * colWidth * Math.tan(skew * Math.PI / 180)) * sign;
-
 		let rowHTML = '';
-		let rowIndex = 0;
-
-		while (rowIndex < rows) {
+		for (let rowIndex = 0; rowIndex < totalRows; rowIndex++) {
 			const span = Math.random() < 0.2 ? Math.floor(Math.random() * (3 - 2 + 1)) + 2 : 1;
-			const actualSpan = Math.min(span, rows - rowIndex);
+			const actualSpan = Math.min(span, totalRows - rowIndex);
+			const yPos = (rowIndex < rows ? rowIndex : rowIndex - rows) * rowHeight + (rowIndex >= rows ? height : 0);
 			rowHTML += `
-				<rect width="${colWidth}" height="${rowHeight * actualSpan}" x="${colWidth * colIndex}" y="${rowHeight * rowIndex}" fill="hsl(${
+				<rect width="${colWidth}" height="${rowHeight * actualSpan}" x="${colWidth * colIndex}" y="${yPos}" fill="hsl(${
 					random(startHue, endHue)}, ${random(50, 80)}%, ${random(50, 80)}%)">
 				</rect>`;
-			rowIndex += actualSpan;
-		}
-//  scale(1 ${1 + (Math.abs(skew) / 65 * 0.3)})
-		// return `<g transform="skewY(${skew}) translate(0, ${translation})">${rowHTML}</g>`;
-		return `<g>${rowHTML}</g>`;
+				rowIndex += actualSpan - 1;
+			}
+		const translateY = skew > 0 ? -height : 0;
+		return `<g transform="skewY(${skew}) translate(0, ${translateY})">${rowHTML}</g>`;
 	}).join('');
 }
