6f1b5dfd76e03bbf125a003a0d2b77cc554ad01f
Mads Stoumann <stoumann@gmail.com>
2024-08-06 17:36:17 +0200

    X Offset

--- art/homage-square/index.js
+++ art/homage-square/index.js
@@ -8,6 +8,7 @@ const storageKey = 'homagesquare';
 const svg = document.getElementById('svg');
 
 GUI.addRange('Squares', 4, '', { min: 2, max: 20, name: 'squares' });
+GUI.addRange('X Offset', 0, '', { min: -1, max: 1, step: 0.01, name: 'xoffset' });
 GUI.addRange('Y Offset', 0.25, '', { min: -1, max: 1, step: 0.01, name: 'yoffset' });
 GUI.addRange('Spacing', 1.5, '', { min: 1, max: 3, step: 0.1, name: 'spacing' });
 GUI.addColor('Color', '#ff0055', '', { name: 'color' });
@@ -25,6 +26,7 @@ function homageSquare(svg, controls) {
 	const [h, s, l] = hexToHSL(controls.color.value);
 	const reverse = controls.reverse.checked;
 	const lightnessFactor = controls.lightnessfactor.valueAsNumber;
+	const xOffsetFactor = controls.xoffset.valueAsNumber;
 	const yOffsetFactor = controls.yoffset.valueAsNumber;
 	const spacingFactor = controls.spacing.valueAsNumber;
 	const maxSquareSize = Math.min(width, height);
@@ -37,13 +39,14 @@ function homageSquare(svg, controls) {
 		const baseFactor = reverse ? ((squares - index - 1) / (squares - 1)) : (index / (squares - 1));
 		const currentL = minLightness + (maxLightness - minLightness) * baseFactor;
 		const squareSize = maxSquareSize - (index * decrement);
+		const xOffset = index * decrement * xOffsetFactor;
 		const yOffset = index * decrement * yOffsetFactor;
 		if (index === 0) svg.style.backgroundColor = `hsl(${h}, ${s}%, ${currentL}%)`;
 		return `
 			<rect
 				width="${squareSize}"
 				height="${squareSize}"
-				x="${(width - squareSize) / 2}"
+				x="${(width - squareSize) / 2 + xOffset}"
 				y="${(height - squareSize) / 2 + yOffset}"
 				fill="hsl(${h}, ${s}%, ${currentL}%)">
 			</rect>`;
