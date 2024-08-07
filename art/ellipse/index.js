d173192bbc4223fb3b70021e3cf4a9f1c9c58c53
Mads Stoumann <stoumann@gmail.com>
2024-08-07 07:13:36 +0200

    Naming bug

--- art/ellipse/index.js
+++ art/ellipse/index.js
@@ -30,7 +30,7 @@ function ellipse(svg, controls) {
 
   svg.innerHTML = `<g transform="translate(${width / 2} ${height / 2}) scale(${scale})">
     <circle cx="0" cy="0" r="50" stroke-width="${strokeWidth}" fill="none"></circle>
-    ${Array.from({ length: numEllipses }).map((_sphere, index) => `<g transform="rotate(${360/numSpheres * index})">
+    ${Array.from({ length: numEllipses }).map((_sphere, index) => `<g transform="rotate(${360/numEllipses * index})">
 			<ellipse cx="0" cy="0" rx="15" ry="50" stroke-width="${strokeWidth}" fill="hsla(${H}, ${S}%, ${L}%, .1)"></ellipse>
 			<line x1="0" y1="-50" x2="0" y2="50" stroke-width="${strokeWidth}"></line>
 		</g>
