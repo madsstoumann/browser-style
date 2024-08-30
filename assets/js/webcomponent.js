e8761dd73896e73d11c6d15dbd4c78e2fea9e37e
Mads Stoumann <stoumann@gmail.com>
2024-08-30 08:58:26 +0200

    Documentation and update to genric WebComponentTemplate

--- assets/js/webcomponent.js
+++ assets/js/webcomponent.js
@@ -2,7 +2,7 @@
  * Web Component Template
  * @author Mads Stoumann
  * @version 1.0.00
- * @summary 29-08-2024
+ * @summary 30-08-2024
  * @class
  * @extends {HTMLElement}
  */
@@ -10,26 +10,36 @@ export class WebComponentTemplate extends HTMLElement {
 	constructor() {
 		super();
 
+		// Determine if Shadow DOM should be used
 		const useShadow = !this.hasAttribute('no-shadow');
+		
+		// Determine if styles should be included
 		const noStyles = this.hasAttribute('no-styles');
+		
+		// Attach shadow root or use the element itself
 		this.root = useShadow ? this.attachShadow({ mode: 'open' }) : this;
 
+		// If not using Shadow DOM, prepare to manage slots manually
 		if (!useShadow) {
 			this.getSlots();
 		}
+
+		// If using Shadow DOM and styles are not excluded, set up stylesheets
 		if (useShadow && !noStyles) {
 			this.stylesheet = new CSSStyleSheet();
-      this.root.adoptedStyleSheets = [this.stylesheet];
-    }
+			this.root.adoptedStyleSheets = [this.stylesheet];
+		}
 	}
 
 	connectedCallback() {
+		// Append slots if not using Shadow DOM
 		if (!this.shadowRoot) {
 			this.appendSlots();
 		}
 	}
 
 	appendSlots() {
+		// Method to insert slotted elements into the appropriate places in the template
 		if (this.slots) {
 			this.slots.childNodes.forEach((node) => {
 				const name = node.getAttribute('slot');
@@ -42,15 +52,17 @@ export class WebComponentTemplate extends HTMLElement {
 	}
 
 	getSlots() {
+		// Method to collect slotted elements into a DocumentFragment
 		this.slots = new DocumentFragment();
 		this.querySelectorAll('[slot]').forEach((el) => {
-			this.slots.appendChild(el.cloneNode(true));
+			this.slots.appendChild(el.cloneNode(true)); // Clone nodes to prevent moving them from the light DOM
 		});
 	}
 
 	static mount() {
-    if (!customElements.get(this.tagName)) {
-      customElements.define(this.tagName, this);
-    }
-  }
+		// Define the custom element if it hasn't been defined yet
+		if (!customElements.get(this.tagName)) {
+			customElements.define(this.tagName, this);
+		}
+	}
 }
