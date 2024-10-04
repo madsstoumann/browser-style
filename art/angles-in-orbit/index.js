097c7f51076a1aa9d5a2e4ba0d84b2eac5c88781
Mads Stoumann <stoumann@gmail.com>
2024-10-04 08:04:12 +0200

    Angles in Orbit

--- art/angles-in-orbit/index.js
+++ art/angles-in-orbit/index.js
@@ -4,15 +4,15 @@ import { interpolateColor } from '../../assets/js/color.js';
 import GuiControl from '/ui/gui-control/index.js';
 
 const GUI = document.querySelector('gui-control');
-const storageKey = 'circlecircles';
+const storageKey = 'anglesinorbit';
 const svg = document.getElementById('svg');
 
 GUI.addRange('Triangles', 12, '', { min: 3, max: 100, name: 'triangles' });
-GUI.addRange('Inner Gap', 0, '', { min: 0, max: 25, name: 'innergap', value: 0 });
+GUI.addRange('Inner Gap', 0, '', { min: 0, max: 25, name: 'inner', value: 0 });
 GUI.addColor('Line color', '#805280', '', { name: 'stroke' });
-GUI.addRange('Line width', 0.1, '', { min: 0, max: 1, step: 0.01, name: 'linestrokewidth' });
-GUI.addColor('Fill Start', '#7b3769', '', { name: 'fillstart' });
-GUI.addColor('Fill End', '#1a1a3c', '', { name: 'fillend' });
+GUI.addRange('Line width', 0, '', { min: 0, max: 1, step: 0.01, name: 'linestrokewidth', value: 0 });
+GUI.addColor('Fill Start', '#7d366b', '', { name: 'fillstart' });
+GUI.addColor('Fill End', '#d9a6cc', '', { name: 'fillend' });
 GUI.addRange('Scale', 0.95, '', { min: 0, max: 2, step: 0.025, name: 'scale' });
 commonConfig(GUI, '#282852');
 GUI.addEventListener('gui-input', (event) => handleGuiEvent(event, svg, GUI, storageKey, anglesInOrbit));
@@ -25,7 +25,7 @@ function anglesInOrbit(svg, controls) {
 	const triangles = controls.triangles.valueAsNumber;
 	const fillEnd = controls.fillend.value;
 	const fillStart = controls.fillstart.value;
-	const innerGap = controls.innergap.valueAsNumber;
+	const inner = controls.inner.valueAsNumber;
 	const lineStrokeWidth = controls.linestrokewidth.valueAsNumber;
 	
 	const stroke = controls.stroke.value;
@@ -42,13 +42,15 @@ function anglesInOrbit(svg, controls) {
 	const polygons = Array.from({ length: triangles }).map((_, index) => {
 		const angle = index * angleStep;
 		const factor = index / (triangles - 1);
-		const fillColor = interpolateColor(fillStart, fillEnd, factor );
+		const fillColor = index < triangles / 2 
+						? interpolateColor(fillStart, fillEnd, factor) 
+						: interpolateColor(fillEnd, fillStart, factor);
 		const prevAngle = (index - 1 + triangles) % triangles * angleStep;
 		const x1 = radius * Math.cos(angle);
 		const y1 = radius * Math.sin(angle);
 		const x2 = radius * Math.cos(prevAngle) / 2;
 		const y2 = radius * Math.sin(prevAngle) / 2;
-		return `<polygon points="0,0 ${x1},${y1} ${x2},${y2}" fill="${fillColor}"/>`;
+		return `<polygon points="${inner * Math.cos(angle)},${inner * Math.sin(angle)} ${x1},${y1} ${x2},${y2}" fill="${fillColor}"/>`;
 	});
 
 	svg.innerHTML = `
