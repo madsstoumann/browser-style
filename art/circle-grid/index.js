911a11b1101b248eb31cb34b222788f1798c963c
Mads Stoumann <stoumann@gmail.com>
2024-12-08 08:59:27 +0100

    Add grid color and width options to circle grid configuration

--- art/circle-grid/index.js
+++ art/circle-grid/index.js
@@ -9,6 +9,8 @@ const svg = document.getElementById('svg');
 GUI.addRange('Cols', 4, '', { min: 10, max: 50, name: 'columns' });
 GUI.addColor('Line color', '#f075f0', '', { name: 'stroke' });
 GUI.addRange('Line width', 0.5, '', { min: 0.01, max: 1, step: 0.01, name: 'linestrokewidth' });
+GUI.addColor('Grid color', '#cccccc', '', { name: 'strokegrid' });
+GUI.addRange('Grid width', 0.05, '', { min: 0, max: .5, step: 0.01, name: 'gridstrokewidth' });
 GUI.addCheckbox('Fixed angles', '0', '', { name: 'angles', checked: 'checked' });
 
 commonConfig(GUI, '#ffffff');
@@ -21,6 +23,8 @@ function circleGrid(svg, controls) {
 	const { width, height } = getViewBox(svg);
 	const angleFixed = controls.angles.checked;
 	const columns = controls.columns.valueAsNumber;
+	const gridColor = controls.strokegrid.value;
+	const gridWidth = controls.gridstrokewidth.valueAsNumber;
 	const stroke = controls.stroke.value;
 	const strokeWidth = controls.linestrokewidth.valueAsNumber;
 
@@ -39,6 +43,7 @@ function circleGrid(svg, controls) {
 
 		return `
 			<g transform="translate(${colIndex * boxSize}, ${rowIndex * boxSize}) rotate(${angle}, ${boxSize / 2}, ${boxSize / 2})" clip-path="url(#${clipPathId})">
+				<rect width="${boxSize}" height="${boxSize}" fill="none" stroke="${gridColor}" stroke-width="${gridWidth}" />
 				<circle cx="0" cy="0" r="${boxSize / 2}" fill="none" stroke="${stroke}" stroke-width="${strokeWidth}" />
 				<circle cx="${boxSize}" cy="${boxSize}" r="${boxSize / 2}" fill="none" stroke="${stroke}" stroke-width="${strokeWidth}" />
 			</g>`;
