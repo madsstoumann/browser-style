add90e77718f32531d5cc4d9b9f60c18094e1ae8
Mads Stoumann <stoumann@gmail.com>
2025-03-04 10:18:26 +0100

    Refactor GUI tabs component: update active tab handling, improve accessibility attributes, and streamline event listener setup

--- ui/gui-tabs/index.js
+++ ui/gui-tabs/index.js
@@ -1,6 +1,5 @@
 const styles = await fetch(new URL('./index.css', import.meta.url).href).then(r => r.text());
 export default class GuiTabs extends HTMLElement {
-	#active;
 	#initialized;
 	#panels;
 	#root;
@@ -8,9 +7,7 @@ export default class GuiTabs extends HTMLElement {
 	
 	constructor() {
 		super();
-		this.#active = 0;
 		this.#initialized = false;
-
 		this.#root = this.attachShadow({mode: 'open'});
 		const sheet = new CSSStyleSheet();
 		sheet.replaceSync(styles);
@@ -20,18 +17,19 @@ export default class GuiTabs extends HTMLElement {
 	connectedCallback() {
 		if (!this.#initialized) {
 			this.#render();
-			this.setupEventListeners();
+			this.#setupEventListeners();
+			this.#setTab(this.#root.querySelector('[aria-selected="true"]'));
 			this.#initialized = true;
 		}
 	}
 
 	#render() {
-		const tabs = Array.from(this.querySelectorAll('gui-tab')).map(tab => (tab.id = tab.id || `tab-${crypto.randomUUID()}`, tab));
+		const tabs = Array.from(this.querySelectorAll('gui-tab')).map(tab => (tab.id = tab.id || crypto.randomUUID(), tab));
 		this.#root.innerHTML = `
 			<nav role="tablist">
-				${tabs.map((tab, index) => {
+				${tabs.map((tab) => {
 					return `
-						<a href="#${tab.id}" role="tab" aria-selected="${index === this.#active}" aria-controls="${tab.id}">
+						<a href="#${tab.id}" id="label-${tab.id}" role="tab" aria-selected="${tab.hasAttribute('active') ? true : false}" aria-controls="tab-${tab.id}">
 							${tab.getAttribute('label')}
 						</a>`;
 				}
@@ -40,7 +38,7 @@ export default class GuiTabs extends HTMLElement {
 			<div part="panels">
 				${tabs.map((tab) => {
 					return `
-						<div role="tabpanel" id="${tab.id}" aria-labelledby="${tab.id}">
+						<div role="tabpanel" id="tab-${tab.id}" aria-labelledby="label-${tab.id}" hidden>
 							${tab.innerHTML}
 						</div>
 					`;
@@ -48,23 +46,24 @@ export default class GuiTabs extends HTMLElement {
 			</div>`
 	}
 
-	setupEventListeners() {
+	#setupEventListeners() {
 		this.#tabs = Array.from(this.#root.querySelectorAll('a[role="tab"]'));
 		this.#panels = Array.from(this.#root.querySelectorAll('div[role="tabpanel"]'));
 		this.#tabs.forEach((tab) => {
 			tab.addEventListener('click', e => {
 				e.preventDefault();
-				const panel = this.#panels.find(panel => panel.id == tab.getAttribute('aria-controls'));
-				if (panel) {
-					panel.scrollIntoView({
-						block: 'nearest',
-						inline: 'start'
-					});
-				}
+				this.#setTab(tab);
 			});
+		});
+	}
+
+	#setTab(tab) {
+		this.#panels.forEach(panel => {
+			const active = panel.id == tab.getAttribute('aria-controls');
+			panel.hidden = !active;
+			panel.setAttribute('aria-selected', active);
 		});
 	}
 }
 
-// Register custom elements
 customElements.define('gui-tabs', GuiTabs);
